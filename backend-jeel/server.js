fe024dcd4b035ba729deba2a4953140ab30642c1
jeel shah <jeelshah911@gmail.com>
2024-08-25 10:25:38 -0600

    commit

--- backend-jeel/server.js
+++ backend-jeel/server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
-const resumeRoutes = require('./routes/resumeroutes');
+const resumeroutes = require('./routes/resumeroutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//middleware-important for express(main things)
-app.use(bodyParser.json()); // Parse JSON bodies
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
 
+// Serve static files from the 'frontend' directory
+app.use(express.static(path.join(__dirname, '../frontend')));
 
+// API routes for handling resumes
+//app.use('/api/resumes', resumeroutes);
 
-
+// Serve the frontend HTML file for any non-API route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 
 // Start the server
 app.listen(PORT, () => {
